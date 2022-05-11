df6637e0ed779329a4d616ce0fcc4492f076e4c6
dmitriy-say <dsay8585@gmail.com>
2022-05-11 22:16:52 +0300

    ^Practice 2 - class Task created.

--- src/main/java/ua/edu/sumdu/j2se/say/tasks/Main.java
+++ src/main/java/ua/edu/sumdu/j2se/say/tasks/Main.java
@@ -2,5 +2,8 @@ package ua.edu.sumdu.j2se.say.tasks;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello");
+
+		Task task = new Task("Running", 25);
+		System.out.println(task);
 	}
 }
